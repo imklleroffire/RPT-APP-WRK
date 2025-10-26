517b794de43dac99d6b2e8c2f500cbb3d94670c6
imklleroffire <ruhanchaudhary@gmail.com>
2025-10-26 15:44:17 -0700

    Fix app.config for EAS Build

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,3 @@
-import 'dotenv/config';
-
 export default {
   expo: {
     name: 'RPT App',
@@ -17,7 +15,6 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.amtkumar.rptapp',
-      buildNumber: '1',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSCameraUsageDescription: 'This app needs access to the camera to upload exercise images.',
@@ -41,12 +38,6 @@ export default {
       policy: 'appVersion',
     },
     extra: {
-      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
       eas: {
         projectId: 'e29930f6-e432-4e09-beb1-4859ca3d3019',
       },

698d21343c7399fd1bbb91c09e8e3ada47c30514
imklleroffire <ruhanchaudhary@gmail.com>
2025-10-26 19:56:43 -0700

    Use absolute minimal app.config for production build

--- app.config.backup.ts
+++ app.config.backup.ts
@@ -5,17 +5,32 @@ export default {
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    scheme: 'rptapp',
     splash: {
       image: './assets/splash.png',
       resizeMode: 'contain',
       backgroundColor: '#ffffff',
     },
+    assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.amtkumar.rptapp',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
+        NSCameraUsageDescription: 'This app needs access to the camera to upload exercise images.',
+        NSPhotoLibraryUsageDescription: 'This app needs access to your photo library to select exercise images.',
       },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'com.amtkumar.rptapp',
+    },
+    web: {
+      favicon: './assets/favicon.png',
     },
     extra: {
       eas: {
@@ -24,5 +39,4 @@ export default {
     },
     plugins: ['expo-router'],
   },
-};
-
+}; 

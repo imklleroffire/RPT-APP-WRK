32cf0ad8b6ba339758cf70235d08812b3a03bf7d
imklleroffire <ruhanchaudhary@gmail.com>
2025-10-26 17:56:18 -0700

    Configure Reanimated plugin properly

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin'
+      ['react-native-reanimated/plugin', {
+        relativeSourceLocation: true,
+      }],
     ],
   };
 }; 
